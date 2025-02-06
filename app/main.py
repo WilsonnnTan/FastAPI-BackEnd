d3babbc6b30facd5fe909dc89ab1a9b052461ab1
Wilson0024 <wilsonangelietann@gmail.com>
2025-02-06 15:53:47 +0700

    feat: add route

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
-from auth import user
-from database import engine, Base
+from fastapi.responses import RedirectResponse
+from app.auth import user
+from app.database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(user.router)
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
