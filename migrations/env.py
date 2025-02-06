7242d463d87a1b20a0b6bf2237cb61265c6ac5a4
Wilson0024 <wilsonangelietann@gmail.com>
2025-02-06 15:45:39 +0700

    feat: add migration scripts

--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,7 +6,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from models.user import User
+
+# Read environment variable for DATABASE_URL
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +23,11 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+from app.database import Base
+from app.models import user
+
+target_metadata = user.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
